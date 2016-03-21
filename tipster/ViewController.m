b51a34398889869d0b08a839a92bd2dccbf01571
Yanze LIU <yanzeliu.france@gmail.com>
2016-03-21 11:12:06 -0700

    first commit

--- tipster/ViewController.m
+++ tipster/ViewController.m
@@ -9,10 +9,19 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalTipNumLabel;
+@property (weak, nonatomic) IBOutlet UILabel *tipPerPerspnNumLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalToPayNumLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalPerPersonNumLabel;
+
+
 
 @end
 
 @implementation ViewController
+int counter = 0;
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -23,5 +32,39 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)buttonClicked:(UIButton *)sender {
+    
+    if ([self.totalLabel.text  isEqual: @"0"]) {
+        self.totalLabel.text = [(UIButton *) sender currentTitle];
+//        self.totalLabel.text = [NSString stringWithFormat:@"%@%@", self.totalLabel.text, [(UIButton *)sender currentTitle]];
+    }
+    
+    else if ([[(UIButton *) sender currentTitle] isEqual:@"c"]) {
+        self.totalLabel.text = @"0";
+        counter = 0;
+    }
+    
+    else if([[(UIButton *) sender currentTitle] isEqual:@"."]){
+        if (counter == 0) {
+            counter++;
+            self.totalLabel.text = [NSString stringWithFormat:@"%@%@", self.totalLabel.text, [(UIButton *)sender currentTitle]];
+        }
+        else{
+            return;
+        }
+        
+    }
+    
+    else{
+        self.totalLabel.text = [NSString stringWithFormat:@"%@%@", self.totalLabel.text, [(UIButton *)sender currentTitle]];
+    }
+    
+    
+//    NSLog(@"%@", self.totalLabel.text);
+    
+}
+
+
 
 @end
