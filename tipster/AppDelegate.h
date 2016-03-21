f33a2969faa0341ddc5d80a550000fb989492aff
Yanze LIU <yanzeliu.france@gmail.com>
2016-03-20 17:49:22 -0700

    Initial Commit

---
+++ tipster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tipster
+//
+//  Created by yanze on 3/20/16.
+//  Copyright © 2016 yanzeliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tipster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipster
+//
+//  Created by yanze on 3/20/16.
+//  Copyright © 2016 yanzeliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
