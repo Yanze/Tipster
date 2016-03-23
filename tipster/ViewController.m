a306d1d5737a575e7d937508b772ccdeb1e25c18
Yanze LIU <yanzeliu.france@gmail.com>
2016-03-23 13:26:26 -0700

    resolved bug about key zero

--- tipster/ViewController.m
+++ tipster/ViewController.m
@@ -32,7 +32,7 @@
 
 @implementation ViewController
 int counter = 0;
-int totalBill = 0;
+float totalBill = 0.00;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -61,31 +61,51 @@ int totalBill = 0;
 
 
 //MARK: button clicked function
+    BOOL userIsInTheMiddleOfTypingNumber = NO;
 - (IBAction)buttonClicked:(UIButton *)sender {
-    if ([self.totalLabel.text  isEqual: @"0"]) {
-        self.totalLabel.text = [(UIButton *) sender currentTitle];
-    }
     
-    else if([[(UIButton *) sender currentTitle] isEqual:@"."]){
-        if (counter == 0) {
-            counter++;
-            self.totalLabel.text = [NSString stringWithFormat:@"%@%@", self.totalLabel.text, [(UIButton *)sender currentTitle]];
-        }
-        else{
-            return;
-        }
-        
-    }
-    
-    else{
+    if(userIsInTheMiddleOfTypingNumber){
         self.totalLabel.text = [NSString stringWithFormat:@"%@%@", self.totalLabel.text, [(UIButton *)sender currentTitle]];
     }
+    else {
+        self.totalLabel.text = [(UIButton *) sender currentTitle];
+        userIsInTheMiddleOfTypingNumber = YES;
+    }
+    
+//    if ([self.totalLabel.text  isEqual: @"0"]) {
+//        self.totalLabel.text = [(UIButton *) sender currentTitle];
+//    }
+    
+//    
+//    else if([[(UIButton *) sender currentTitle] isEqual:@"."]){
+//        if (counter == 0) {
+//            counter++;
+//            self.totalLabel.text = [NSString stringWithFormat:@"%@%@", self.totalLabel.text, [(UIButton *)sender currentTitle]];
+//        }
+//        else{
+//            return;
+//        }
+//        
+//    }
+//    
+//    else{
+//        self.totalLabel.text = [NSString stringWithFormat:@"%@%@", self.totalLabel.text, [(UIButton *)sender currentTitle]];
+//    }
     
     totalBill = [self.totalLabel.text intValue];
     [self updateTipLabel];
     [self updatePerPersonLabel];
     
 }
+
+- (IBAction)zeroClicked:(UIButton *)sender {
+    if(userIsInTheMiddleOfTypingNumber){
+        self.totalLabel.text = [NSString stringWithFormat:@"%@%@", self.totalLabel.text, [(UIButton *)sender currentTitle]];
+    }
+}
+
+
+
 
 - (IBAction)cClicked:(UIButton *)sender {
     self.tipPercentage.text = @"10";
@@ -100,7 +120,10 @@ int totalBill = 0;
     self.totalToPayNumLabel.text = @"0";
     self.totalPerPersonNumLabel.text = @"0";
     counter = 0;
-    totalBill = 0;
+    totalBill = 0.00;
+    
+    userIsInTheMiddleOfTypingNumber = NO;
+    
     [self updatePerPersonLabel];
     [self updateTipLabel];
 }
@@ -111,17 +134,19 @@ int totalBill = 0;
 - (IBAction)tipSlider:(UISlider *)sender {
     self.tipPercentage.text = [NSString stringWithFormat:@"%d", (int)sender.value];
     [self updateTipLabel];
+    [self updatePerPersonLabel];
 }
 
 
 - (IBAction)groupSizeSlider:(UISlider *)sender {
     self.groupSizeLabel.text = [NSString stringWithFormat:@"%d", (int)sender.value];
+    [self updateTipLabel];
     [self updatePerPersonLabel];
 }
 
 
 -(void)updatePerPersonLabel{
-    float tip = (float)((int)totalBill * (int)self.tipSlider.value) / (int)100;
+    float tip = (float)(totalBill * (int)self.tipSlider.value) / (int)100;
     
     // tip per person
     float tipPerPerson = (float)(float)tip / (int)self.groupSlider.value;
@@ -135,7 +160,7 @@ int totalBill = 0;
 
 
 - (void)updateTipLabel{
-    float tip = (float)((int)totalBill * (int)self.tipSlider.value) / (int)100;
+    float tip = (float)((float)totalBill * (int)self.tipSlider.value) / (int)100;
     self.totalTipNumLabel.text = [NSString stringWithFormat:@"$ %.2f", tip];
     float totalWithTip = tip + (float)totalBill;
     self.totalToPayNumLabel.text = [NSString stringWithFormat:@"$ %.2f", totalWithTip];
